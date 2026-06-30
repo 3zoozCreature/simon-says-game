7f7996d74df81b59bdcb70f5d86315fcced6925d
Abdulaziz Makhlooq <abdulaziz.makhlooq@gmail.com>
2026-06-30 12:09:35 +0300

    added sounds variables

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 /*-------------------------------- Constants --------------------------------*/
 const colorArr = ['red', 'blue', 'green', 'yellow']
 
+const gameSounds = {
+  green: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
+  red: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
+  blue: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"),
+  yellow: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"),
+  error: new Audio("https://google.com") 
+};
+
 /*-------------------------------- Variables --------------------------------*/
 let gameSequence = []
 
